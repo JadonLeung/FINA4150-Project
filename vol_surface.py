dad72464084b0c9e8a7578cf4bad9ceb18249d07
JadonLeung <113084964+JadonLeung@users.noreply.github.com>
2023-11-06 13:58:24 +0800

    commit vol_curve class

--- vol_surface.py
+++ vol_surface.py
@@ -1,9 +1,14 @@
 import numpy as np
 import scipy.optimize as opt
 
+'''
+
+'''
 class vol_curve:
-    def __init__(self):
-        pass
+    def __init__(self, vol: list, logmoneyness: list, atm_vol: float):
+        self.sigma = vol
+        self.K = logmoneyness
+        self.sigmaATM = atm_vol
 
     def _obj(params, sigma, K, sigmaATM):
         delta, gamma, kappa = params
@@ -11,20 +16,10 @@ class vol_curve:
         K = np.array(K)
         return sum((sigma - sigmaATM - delta * (np.tanh(kappa * K) / kappa) - gamma / 2 * (np.tanh(kappa * K) / kappa) ** 2) ** 2)
 
-    def fit():
-        pass
+    def fit(self, method='SLSQP', ini_guess=[1, 1, 1]):
+        self.result = opt.minimize(self._obj, np.array(ini_guess), method=method, args = (self.sigma, self.K, self.sigmaATM))
+        self.delta, self.gamma, self.kappa = self.result.x
 
-def vol_curve_obj(params, sigma, K, sigmaATM):
-    delta, gamma, kappa = params
-    sigma = np.array(sigma)
-    K = np.array(K)
-    return sum((sigma - sigmaATM - delta * (np.tanh(kappa * K) / kappa) - gamma / 2 * (np.tanh(kappa * K) / kappa) ** 2) ** 2)
-
-def vol_curve_get(params, K, sigmaATM):
-    delta, gamma, kappa = params
-    K = np.array(K)
-    return sigmaATM + delta * (np.tanh(kappa * K) / kappa) + gamma / 2 * (np.tanh(kappa * K) / kappa) ** 2
-
-def vol_curve_optimize(vol, K, volATM):
-    res = opt.minimize(vol_curve_obj, np.array([1, 1, 1]), method='SLSQP', args = (vol, K, volATM))
-    return res.x
+    def get(self, logmoneyness: float or list):
+        K = np.array(logmoneyness)
+        return self.sigmaATM + self.delta * (np.tanh(self.kappa * K) / self.kappa) + self.gamma / 2 * (np.tanh(self.kappa * K) / self.kappa) ** 2
